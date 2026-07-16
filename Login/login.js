11a2bb2d1cb83a30911de3b2b45a613ea983b98d
buddahcoder69 <dylanjpagano@gmail.com>
2026-07-16 09:57:30 -0400

    v0.1 pr2 patch 1

---
+++ Login/login.js
@@ -0,0 +1,9 @@
+let submitbutton = document.querySelector('.submit');
+submitbutton.addEventListener('click', function(e) {
+  e.preventDefault();
+  let form = document.querySelector('.form');
+  let formData = new FormData(form);
+  let dataObject = Object.fromEntries(formData.entries());
+  // Output: { username: "john_doe", password: "johnpasswprd" } this is the user input
+  
+})
